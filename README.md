0a290e21aa0f3ac36566a6d652b0e8819edc636a
Harry Scells <harryscells@gmail.com>
2016-08-09 14:51:51 +1000

    updated formula to use sha256

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-homebrew-bpiwowar
-=================
+# homebrew tap for trec\_eval
+
+This repository contains a homebrew formula for installing trec\_eval through homebrew!
+
+## Installing
+
+ - `brew tap hscells/homebrew-trec-eval`
+
+ - `brew install trec_eval`
--- trec_eval.rb
+++ trec_eval.rb
@@ -3,7 +3,7 @@ require "formula"
 class TrecEval < Formula
   homepage "http://trec.nist.gov/trec_eval/"
   url "http://trec.nist.gov/trec_eval/trec_eval.8.1.tar.gz"
-  sha1 "b983948d797ae9ca80845b90742cdb7176ace7d5"
+  sha256 "38147c4a0fa5d13b09092b0f8dd55142902ae2a4bc6e34db03fadb78f28f612e"
 
 
   def install
